0d75c75473e2766ff22425a8ebc89ebf57363b82
kwiats <pawuskwiatkowski@gmail.com>
2023-07-21 21:14:16 +0200

    dodano serwis oraz migracje

--- api/photos/admin.py
+++ api/photos/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from photos.models import Photo
+
+from photos.models import Photo, PhotoPositions
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ["photo", "created_date", "column_id", "order"]
+
+
+class PhotoPositionsAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ["uuid", "created_date"]
+
 
 # Register your models here.
-admin.site.register(Photo)
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(PhotoPositions, PhotoPositionsAdmin)
--- api/photos/models.py
+++ api/photos/models.py
@@ -1,26 +1,25 @@
-import uuid
+from django.core.validators import FileExtensionValidator
 from django.db import models
+
+from core.models import BaseModel
+
+
+class PhotoPositions(BaseModel):
+    columns = models.JSONField(null=True)
 
 
 def upload_to(instance, filename):
     return "assets/images/{filename}".format(filename=filename)
 
 
-class Photo(models.Model):
-    slug = models.UUIDField(
-        default=uuid.uuid4,
-        editable=False,
-    )
-    photo = models.ImageField(("Photo"), upload_to=upload_to, max_length=100)
-    updated = models.DateTimeField(
-        ("Updated"),
-        auto_now=False,
-        auto_now_add=True,
-    )
-    created = models.DateField(("Created"), auto_now=False, auto_now_add=False)
+class Photo(BaseModel):
+    photo = models.ImageField(upload_to=upload_to, max_length=100, verbose_name='photos',
+                              validators=[FileExtensionValidator(['jpg', 'jpeg'])])
+    column_id = models.IntegerField(default=1)
+    order = models.IntegerField(default=0)
 
     def __str__(self):
-        return f"{self.slug}"
+        return f"{self.uuid} - {self.photo}"
 
     class Meta:
         verbose_name = "photo"
