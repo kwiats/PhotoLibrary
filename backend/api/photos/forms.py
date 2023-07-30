27dfd69b0768f586cba49a5bd5ece011979bbcf2
kwiats <pawuskwiatkowski@gmail.com>
2023-07-30 11:15:54 +0200

    poprawiono strukture aplikacji

--- backend/api/photos/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class PhotoForm(forms.Form):
-    template_name = "photo_form.html"
-    photo = forms.ImageField(label="Photo", required=False)
-    created = forms.DateField(label="Created", required=False)
--- src/app/core/addons/services/spinner.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class SpinnerService {
-  showSpinner$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-}
