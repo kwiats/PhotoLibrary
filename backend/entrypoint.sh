e16c35d728531298307515e2480ce8c28fac700d
kwiats <pawuskwiatkowski@gmail.com>
2023-07-30 15:16:16 +0200

    poprawiono dockera

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 python3 manage.py collectstatic --noinput
-python3 manage.py task_runner &
 ln -s staticfiles backend/static
 python3 -m http.server 9000 &
 gunicorn --config gunicorn-cfg.py core.wsgi
