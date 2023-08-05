a6b7a07e50d146b987acac2c711f65c56db86896
kwiats <pawuskwiatkowski@gmail.com>
2023-08-05 16:51:53 +0200

    poprawiono backend

--- backend/photos/views.py
+++ backend/photos/views.py
@@ -1,12 +1,11 @@
-import json
-
-from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from photos.models import Photo, PhotoPositions
+from photos.models import PhotoPositions
 from photos.serializers import PhotoSerializer, PhotoPositionsSerializer
 from photos.services.photo_service import PhotoService
 
@@ -17,43 +16,51 @@ class PositionsViewSet(viewsets.ModelViewSet):
 
 
 class PhotoView(APIView):
+    authentication_classes = [
+        SessionAuthentication,
+        BasicAuthentication,
+    ]
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         photos = PhotoService.get_photos()
         serializer = PhotoSerializer(photos, many=True)
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
-        files = request.FILES.getlist('file')
-        message = f'Uploaded {len(files)} files to server'
+        files = request.FILES.getlist("file")
+        message = f"Uploaded {len(files)} files to server"
         PhotoService.create_photo(files)
-        return Response({'message': message})
+        return Response({"message": message})
 
     def delete(self, request):
-        message = f'Image is deleted'
-        return Response({'message': message})
+        message = f"Image is deleted"
+        photo_id = request.data.get("photo")
+        PhotoService.delete_photo(photo_id)
+        return Response({"message": message})
 
 
 class PhotoPositionViews(APIView):
     def get(self, request):
-        latest_configuration = PhotoPositions.objects.order_by('-created_date')
+        latest_configuration = PhotoPositions.objects.order_by("-created_date")
         latest_configuration = latest_configuration.first()
 
         if latest_configuration is None:
-            return JsonResponse({'message': 'No configurations found.'}, status=404)
+            return JsonResponse({"message": "No configurations found."}, status=404)
 
         columns = latest_configuration.columns
 
-        return JsonResponse({'columns': columns})
+        return JsonResponse({"columns": columns})
 
     def post(self, request):
         columns = {
-            '1': request.data.get('1'),
-            '2': request.data.get('2'),
-            '3': request.data.get('3')
+            "1": request.data.get("1"),
+            "2": request.data.get("2"),
+            "3": request.data.get("3"),
         }
 
         PhotoService.update_photo(columns=columns)
 
         PhotoPositions.objects.create(columns=columns)
 
-        return JsonResponse({'message': 'Configuration saved successfully.'})
+        return JsonResponse({"message": "Configuration saved successfully."})
