98a80b79a36898772a1c56e94d2322f08fdd26e1
kwiats <pawuskwiatkowski@gmail.com>
2023-08-06 13:27:26 +0200

    zmiana enviroments file

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // @ts-ignore
-    apiUrl: window['env']['apiUrl'] || 'http://nataliastrzelczyk.com',
+    apiUrl: 'https://api.nataliastrzelczyk.com/',
     version: 'VERSION_NOT_SET',
 };
