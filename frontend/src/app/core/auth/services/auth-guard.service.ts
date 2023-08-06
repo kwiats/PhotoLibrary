e9b4fbe37881e0913218d7754083d2e073e5a417
kwiats <pawuskwiatkowski@gmail.com>
2023-08-06 15:40:16 +0200

    poprawiono authguarda

--- frontend/src/app/core/auth/services/auth-guard.service.ts
+++ frontend/src/app/core/auth/services/auth-guard.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from "@angular/core";
 import jwt_decode from "jwt-decode";
 import {AuthService} from "./auth.service";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 import {Router} from "@angular/router";
+import {HttpService} from "../../common/services/http.service";
 
 
 @Injectable({
@@ -15,27 +16,113 @@ export class AuthGuard {
 
 
     constructor(private authService: AuthService,
-                private router: Router) {
+                private router: Router,
+                private httpService: HttpService) {
     }
 
-    canActivate(): boolean {
-        const access = window.localStorage.getItem('access');
-        const refresh = window.localStorage.getItem('refresh');
-        if (!access) {
-            this.router.navigate(['/', 'auth', 'login']);
-            return false;
-        } else {
-            const accessObject = JSON.parse(access);
-            const jwtContent = jwt_decode(accessObject.idToken || accessObject);
-            // @ts-ignore
-            const isExpired = jwtContent.exp < (new Date().getTime() / 1000);
-            if (!isExpired && refresh) {
-                // @ts-ignore
-                const secondsLeft = jwtContent.exp - (new Date().getTime() / 1000) - 30;
-                this.setUpdate(JSON.parse(refresh), secondsLeft * 1000);
+    async canActivate(): Promise<boolean> {
+        return new Promise((resolve) => {
+            this.getAuthToken()
+                .then(res => {
+                    if (res) {
+                        this.getRefreshToken()
+                            .then(refresh => {
+                                const jwtContent: any = jwt_decode(res)
+                                const isExpired = jwtContent.exp < (new Date().getTime() / 1000);
+                                if (!isExpired) {
+                                    if (refresh) {
+                                        const secondsLeft = jwtContent.exp - (new Date().getTime() / 1000) - 30;
+                                        this.setUpdate(refresh, secondsLeft);
+                                    }
+                                    resolve(true);
+                                } else {
+                                    if (refresh) {
+                                        const refreshContent: any = jwt_decode(refresh);
+                                        const isRefreshExpired = refreshContent.exp < (new Date().getTime() / 1000);
+                                        if (isRefreshExpired) {
+                                            resolve(false);
+                                        } else {
+                                            this.refreshToken(refresh)
+                                                .subscribe((data) => {
+                                                    this.setTokens(data)
+                                                        .then(() => resolve(true))
+                                                        .catch(() => resolve(false));
+                                                }, () => resolve(false))
+                                        }
+                                    } else {
+                                        resolve(false);
+                                    }
+                                }
+                            })
+                    } else {
+                        this.router.navigate(['/', 'auth', 'login']);
+                        resolve(false);
+                    }
+                })
+        })
+    }
+
+    async getAuthToken(): Promise<string | null> {
+        return new Promise((resolve) => {
+            const accessToken = localStorage.getItem('access');
+            if (accessToken) {
+                try {
+                    const tokenData = jwt_decode<Record<string, unknown>>(accessToken);
+                    // @ts-ignore
+                    if (tokenData.exp >= Date.now() / 1000) {
+                        console.log()
+                        resolve(accessToken);
+                    } else {
+                        resolve(null);
+                    }
+                } catch (e) {
+                    resolve(null);
+                }
+            } else {
+                resolve(null);
             }
-            return !isExpired;
-        }
+        });
+    }
+
+    async getRefreshToken(): Promise<string | null> {
+        return new Promise((resolve) => {
+            const accessToken = localStorage.getItem('refresh');
+            if (accessToken) {
+                try {
+                    const tokenData = jwt_decode<Record<string, unknown>>(accessToken);
+                    // @ts-ignore
+                    if (tokenData.exp >= Date.now() / 1000) {
+                        console.log()
+                        resolve(accessToken);
+                    } else {
+                        resolve(null);
+                    }
+                } catch (e) {
+                    resolve(null);
+                }
+            } else {
+                resolve(null);
+            }
+        });
+    }
+
+    setTokens(data: any): Promise<void> {
+        return new Promise(resolve => {
+            Promise.all([
+                window.localStorage.setItem('access', JSON.stringify(data.access)),
+                window.localStorage.setItem('refresh', JSON.stringify(data.refresh)),
+
+            ])
+                .then(() => {
+                    resolve();
+                });
+        });
+    }
+
+    refreshToken(refreshToken: string): Observable<any> {
+        return this.httpService.post<any>(`api/login/refresh/`, {
+            refresh: refreshToken
+        })
     }
 
     isLogged(): boolean {
--- frontend/src/app/core/common/services/http.service.ts
+++ frontend/src/app/core/common/services/http.service.ts
@@ -6,144 +6,178 @@ import {SpinnerService} from '../../addons/services/spinner.service';
 import jwt_decode from "jwt-decode";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class HttpService {
-  sessionId = '';
+    sessionId = '';
 
-  constructor(
-    private httpClient: HttpClient,
-    private spinnerService: SpinnerService
-  ) {
-  }
-
-  public get apiUrl(): string {
-    return environment.apiUrl;
-  }
-
-  public get<T>(
-    url: string,
-    params?: HttpParams,
-    contentType?: string,
-    successMessage?: string,
-    errorMessage?: string,
-    showPreloader: boolean = true
-  ): Observable<T> {
-    if (showPreloader) {
-      this.spinnerService.showSpinner$.next(true);
+    constructor(
+        private httpClient: HttpClient,
+        private spinnerService: SpinnerService
+    ) {
     }
-    return from(
-      new Promise<T>(async (resolve, reject) => {
-        const headers = await this.getHeaders(contentType || 'json');
-        const options = {
-          headers,
-          params: params || null,
-        };
-        if (contentType === 'file') {
-          // @ts-ignore
-          options.responseType = 'blob' as 'json';
+
+    public get apiUrl(): string {
+        return environment.apiUrl;
+    }
+
+    public get<T>(
+        url: string,
+        params?: HttpParams,
+        contentType?: string,
+        successMessage?: string,
+        errorMessage?: string,
+        showPreloader: boolean = true
+    ): Observable<T> {
+        if (showPreloader) {
+            this.spinnerService.showSpinner$.next(true);
         }
-        this.httpClient.get<T>(`${this.apiUrl}${url}`).subscribe(
-          (res) => {
-            if (successMessage) {
-              console.log('Udało się!', 'success');
-            }
-            this.spinnerService.showSpinner$.next(false);
-            resolve(res);
-          },
-          (err) => {
-            this.spinnerService.showSpinner$.next(false);
-            reject(err);
-          }
+        return from(
+            new Promise<T>(async (resolve, reject) => {
+                const headers = await this.getHeaders(contentType || 'json');
+                const options = {
+                    headers,
+                    params: params || null,
+                };
+                if (contentType === 'file') {
+                    // @ts-ignore
+                    options.responseType = 'blob' as 'json';
+                }
+                this.httpClient.get<T>(`${this.apiUrl}${url}`).subscribe(
+                    (res) => {
+                        if (successMessage) {
+                            console.log('Udało się!', 'success');
+                        }
+                        this.spinnerService.showSpinner$.next(false);
+                        resolve(res);
+                    },
+                    (err) => {
+                        this.spinnerService.showSpinner$.next(false);
+                        reject(err);
+                    }
+                );
+            })
         );
-      })
-    );
-  }
-
-  public post<T>(url: string, data: T, contentType?: string): Observable<T> {
-    return from(new Promise<T>(async (resolve, reject) => {
-      const headers = await this.getHeaders(contentType || 'json');
-      this.httpClient
-        .post<T>(`${this.apiUrl}${url}`, data, {
-          headers
-        })
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => reject(err));
-    }));
-  }
-  public put<T>(url: string, data: T): Observable<T> {
-    return from(new Promise<T>(async (resolve, reject) => {
-      const headers = await this.getHeaders();
-      this.httpClient
-        .put<T>(`${this.apiUrl}${url}`, data, {
-          headers
-        })
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => reject(err));
-    }));
-  }
-  public patch<T>(url: string, data: T): Observable<T> {
-    return from(new Promise<T>(async (resolve, reject) => {
-      const headers = await this.getHeaders();
-      this.httpClient
-        .patch<T>(`${this.apiUrl}${url}`, data, {
-          headers
-        })
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => reject(err));
-    }));
-  }
-  public delete<T>(url: string): Observable<T> {
-    return from(new Promise<T>(async (resolve, reject) => {
-      const headers = await this.getHeaders();
-      this.httpClient
-        .delete<T>(`${this.apiUrl}${url}`, {
-          headers
-        })
-        .subscribe(res => {
-          resolve(res);
-        }, (err) => reject(err));
-    }));
-  }
-
-  async getHeaders(type: string = 'json', noAuth: boolean = false): Promise<HttpHeaders> {
-    let headers = new HttpHeaders();
-    if (!noAuth) {
-      const token = await this.getAuthToken();
-      if (token) {
-        headers = headers.set(
-          'Authorization',
-          `Bearer ${token}`
-        );
-      }
     }
-    if (type === 'json') {
-      headers = headers.set('Content-Type', 'application/json; charset=utf-8;');
-    }
-    return headers;
-  }
 
-  async getAuthToken(): Promise<unknown> {
-    return new Promise(resolve => {
-      // @ts-ignore
-      let accessToken = JSON.parse(localStorage.getItem('access'))
-        if (accessToken) {
-          try {
-            const tokenData = jwt_decode(accessToken) as any;
-            if (tokenData.exp >= (new Date().getTime() / 1000)) {
-              resolve(accessToken);
+    public post<T>(url: string, data: T, contentType?: string): Observable<T> {
+        return from(new Promise<T>(async (resolve, reject) => {
+            const headers = await this.getHeaders(contentType || 'json');
+            this.httpClient
+                .post<T>(`${this.apiUrl}${url}`, data, {
+                    headers
+                })
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => reject(err));
+        }));
+    }
+
+    public put<T>(url: string, data: T): Observable<T> {
+        return from(new Promise<T>(async (resolve, reject) => {
+            const headers = await this.getHeaders();
+            this.httpClient
+                .put<T>(`${this.apiUrl}${url}`, data, {
+                    headers
+                })
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => reject(err));
+        }));
+    }
+
+    public patch<T>(url: string, data: T): Observable<T> {
+        return from(new Promise<T>(async (resolve, reject) => {
+            const headers = await this.getHeaders();
+            this.httpClient
+                .patch<T>(`${this.apiUrl}${url}`, data, {
+                    headers
+                })
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => reject(err));
+        }));
+    }
+
+    public delete<T>(url: string): Observable<T> {
+        return from(new Promise<T>(async (resolve, reject) => {
+            const headers = await this.getHeaders();
+            this.httpClient
+                .delete<T>(`${this.apiUrl}${url}`, {
+                    headers
+                })
+                .subscribe(res => {
+                    resolve(res);
+                }, (err) => reject(err));
+        }));
+    }
+
+    async getHeaders(type: string = 'json'): Promise<HttpHeaders> {
+        this.sessionId =
+            window.localStorage.getItem('sessionId') || 'session-not-set';
+        let headers = new HttpHeaders();
+        const token = window.localStorage.getItem('access');
+        if (token) {
+            const tokenObject = JSON.parse(token);
+            const tokenData = jwt_decode(tokenObject)
+            // @ts-ignore
+            if (tokenData.exp >= Date.now() / 1000) {
+                console.log(tokenObject)
+                headers = headers.set(
+                    'Authorization',
+                    `Bearer ${tokenObject.idToken || tokenObject}`
+                );
             } else {
-              resolve(null)
+                console.log(tokenData)
             }
-          } catch (e) {
-            resolve(null);
-          }
-        } else {
-          resolve(null);
         }
-      })
-  }
+        headers = headers.set('Accept-Language', 'pl');
+        headers = headers.set('SessionId', this.sessionId);
+        return headers;
+    }
+
+    async getAuthToken(): Promise<string | null> {
+        return new Promise((resolve) => {
+            const accessToken = localStorage.getItem('access');
+            if (accessToken) {
+                try {
+                    const tokenData = jwt_decode<Record<string, unknown>>(accessToken);
+                    // @ts-ignore
+                    if (tokenData.exp >= Date.now() / 1000) {
+                        console.log()
+                        resolve(accessToken);
+                    } else {
+                        resolve(null);
+                    }
+                } catch (e) {
+                    resolve(null);
+                }
+            } else {
+                resolve(null);
+            }
+        });
+    }
+
 }
+
+// async getAuthToken(): Promise<unknown> {
+//   return new Promise(resolve => {
+//     // @ts-ignore
+//     let accessToken = JSON.parse(localStorage.getItem('access'))
+//       if (accessToken) {
+//         try {
+//           const tokenData = jwt_decode(accessToken) as any;
+//           if (tokenData.exp >= (new Date().getTime() / 1000)) {
+//             resolve(accessToken);
+//           } else {
+//             resolve(null)
+//           }
+//         } catch (e) {
+//           resolve(null);
+//         }
+//       } else {
+//         resolve(null);
+//       }
+//     })
+// }
+
