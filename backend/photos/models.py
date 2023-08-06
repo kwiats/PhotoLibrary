9c41990f63149a182686a9cb56b862d1f5ce8644
kwiats <pawuskwiatkowski@gmail.com>
2023-08-06 18:08:48 +0200

    Dodano podzial w modelu, skonfigurowano positionowanie zdjec

--- backend/photos/models.py
+++ backend/photos/models.py
@@ -3,16 +3,25 @@ from django.db import models
 
 from core.models import BaseModel
 
-
-class PhotoPositions(BaseModel):
-    columns = models.JSONField(null=True)
+NEW = "NEW"
+POSITIONED = "POSITIONED"
+UNPOSITIONED = "NOT POSITIONED"
 
 
 def upload_to(instance, filename):
     return "assets/images/{filename}".format(filename=filename)
 
 
+class PhotoPositions(BaseModel):
+    columns = models.JSONField(null=True)
+
+
 class Photo(BaseModel):
+    STATUS_PHOTO = [
+        (NEW, "new"),
+        (POSITIONED, "positioned at last configuration"),
+        (UNPOSITIONED, "not positioned"),
+    ]
     photo = models.ImageField(
         upload_to=upload_to,
         max_length=100,
@@ -21,6 +30,7 @@ class Photo(BaseModel):
     )
     column_id = models.IntegerField(default=1)
     order = models.IntegerField(default=0)
+    status = models.CharField(choices=STATUS_PHOTO, max_length=20, default=NEW)
 
     def __str__(self):
         return f"{self.uuid} - {self.photo}"
