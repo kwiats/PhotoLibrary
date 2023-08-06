fd8dce525be738c3d15f22f27e4f307a496fe1ba
kwiats <pawuskwiatkowski@gmail.com>
2023-08-06 12:51:32 +0200

    dodano custom_storage

--- backend/photos/models.py
+++ backend/photos/models.py
@@ -2,6 +2,7 @@ from django.core.validators import FileExtensionValidator
 from django.db import models
 
 from core.models import BaseModel
+from photos.utils.photo_utils import CustomFileSystemStorage
 
 
 class PhotoPositions(BaseModel):
@@ -12,12 +13,16 @@ def upload_to(instance, filename):
     return "assets/images/{filename}".format(filename=filename)
 
 
+custom_storage = CustomFileSystemStorage()
+
+
 class Photo(BaseModel):
     photo = models.ImageField(
         upload_to=upload_to,
         max_length=100,
         verbose_name="photos",
         validators=[FileExtensionValidator(["jpg", "jpeg"])],
+        storage=custom_storage,
     )
     column_id = models.IntegerField(default=1)
     order = models.IntegerField(default=0)
