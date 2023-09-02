de1f8d212c93ffce9af4430b027509942d99fa4b
kwiats <pawuskwiatkowski@gmail.com>
2023-09-02 13:51:45 +0200

    #46 -poprawiono settings

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,10 +4,13 @@
 echo "Collect static files"
 python3 manage.py collectstatic --no-input
 
+# Migrations
+echo "Migrations"
+python3 manage.py migrate --no-input
+
 # Start server
 echo "Starting server"
 # python3 manage.py runserver 0.0.0.0:8000
 gunicorn --config gunicorn-cfg.py core.wsgi
 
 exec "$@"
-
